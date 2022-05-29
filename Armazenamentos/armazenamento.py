b4cd22c84f0445acb32e496c56d9758753371538
João Lucas Alves de Souza <81641961+joaolucas-0000@users.noreply.github.com>
2022-05-29 17:23:14 -0300

    Add files via upload

---
+++ Armazenamentos/armazenamento.py
@@ -0,0 +1,33 @@
+# declaraçao das variveis do programa
+# A, B, C , D, são declardas na atribuição de valores
+
+
+# atribuir o valor 0 à variavel D
+D = 0
+#Exiba na tela o valor de D com frase explicativa
+print("O Valor de D é:", D)
+
+# atribua o valor de 2 a variavel A
+A = 2
+#Exiba na tela o valor de A com frase explicativa
+print("O valor de A é:", A)
+
+# atribua o valor 3 a variavel B
+B = 3
+# Exiba na tela o valor de B cm frase explicativa
+print("O valor de B é:", B)
+
+# atribua o valor 5 na variavel C
+C = 5
+
+#Exibindo o valor de C com frase explicativa
+print("O valor de C é:", C)
+
+# Atualizando o valor da variavel C, assim, acrescentando + 1 
+C = C + 1 
+print("O valor de C (atualizado) é:",C) #Exibe o novo valor de C
+
+# Atribua à variavel D a soma das var A e C(atualizado)
+D = A + C
+print("O valor de D (atualizado) é:", D) # Exibe o valor de D (atualizado) 
+
